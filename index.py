efdf8a8b46807f0b236221d3109362a952c43a15
luntc@bc.edu <luntc@bc.edu>
2014-11-12 01:21:20 -0500

    checks for weather on the index

--- index.py
+++ index.py
@@ -11,8 +11,13 @@ app = Flask(__name__)
 def index():
     ip = request.access_route[0]
     url = "http://api.db-ip.com/addrinfo?addr={}&api_key=5d9dd65d7e5387f647dfbec122537e3e35ddecae".format(ip)
-    addr = requests.get(url)
-    return "You are in"+str(addr.json())
+    addr = requests.get(url).json()
+    
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={},{},{}'.format(addr["city"], addr["stateprov"], addr["country"]))
+    response = requests.get(url)
+    temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
+    temp =  str(temp)
+    return render_template("index.html",city=addr["city"],temp=temp)
 
 @app.route("/<city>")
 def weather(city):
