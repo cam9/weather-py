85b5d2c2d628bb9556899141b2347f941cffcc77
luntc@bc.edu <luntc@bc.edu>
2014-11-11 23:28:47 -0500

    adds weather retreiving route

--- index.py
+++ index.py
@@ -1,8 +1,23 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/" , methods=["GET"])
 def index():
-	return "Elliot is smelly"
+    return "Add a location to the url to see some weather!"
+
+@app.route("/<city>")
+def weather(city):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}'.format(city)
+        response = requests.get(url)
+        temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
+        temp =  str(temp)
+        return render_template("temp.html", city=city, temp=temp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
