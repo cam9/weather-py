4bdb42f165d88507b4888534e443524eeb24393c
luntc@bc.edu <luntc@bc.edu>
2014-11-11 23:31:49 -0500

    adds requests library

--- index.py
+++ index.py
@@ -7,17 +7,17 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/" , methods=["GET"])
+@app.route("/")
 def index():
     return "Add a location to the url to see some weather!"
 
 @app.route("/<city>")
 def weather(city):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}'.format(city)
-        response = requests.get(url)
-        temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
-        temp =  str(temp)
-        return render_template("temp.html", city=city, temp=temp)
+    response = requests.get(url)
+    temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
+    temp =  str(temp)
+    return render_template("temp.html", city=city, temp=temp)
 
 if __name__ == "__main__":
     app.run(debug=True)
