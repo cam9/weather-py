e503e4781332f77278cb7a032f95794ddbe60d95
luntc@bc.edu <luntc@bc.edu>
2014-11-15 20:21:37 -0500

    adds chartjs for forecast to index

--- index.py
+++ index.py
@@ -7,26 +7,40 @@ import requests
 
 app = Flask(__name__)
 
+def forecast(city, state, country, days):
+	url = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&cnt={}&q={},{},{}".format(days, city, state, country)
+	response = requests.get(url)
+	forecast_list = response.json()["list"]
+	return forecast_list
+
+def today_temp(city, state, country):
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={},{},{}'.format(city, state, country)
+	response = requests.get(url)
+	temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
+	temp =  str(int(temp))
+	return temp
+    
+def coarse_addr(ip):
+	url = "http://api.db-ip.com/addrinfo?addr={}&api_key=5d9dd65d7e5387f647dfbec122537e3e35ddecae".format(ip)
+	addr = requests.get(url).json()
+	return addr
+
 @app.route("/")
 def index():
-    ip = request.access_route[0]
-    url = "http://api.db-ip.com/addrinfo?addr={}&api_key=5d9dd65d7e5387f647dfbec122537e3e35ddecae".format(ip)
-    addr = requests.get(url).json()
-    
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={},{},{}'.format(addr["city"], addr["stateprov"], addr["country"])
-    response = requests.get(url)
-    temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
-    temp =  str(int(temp))
-    return render_template("index.html",city=addr["city"],temp=temp)
-    #return render_template("index.html",city="City",temp="55")
+	ip = request.access_route[0]
+	addr = coarse_addr(ip)
+	temp = today_temp(addr["city"], addr["stateprov"], addr["country"])
+	forecast_list = forecast(addr["city"], addr["stateprov"], addr["country"], 7)
+	return render_template("index.html",city=addr["city"],temp=temp, forecast_list=forecast_list)
 
 @app.route("/<city>")
 def weather(city):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}'.format(city)
-    response = requests.get(url)
-    temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
-    temp =  str(temp)
-    return render_template("temp.html", city=city, temp=temp)
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={}'.format(city)
+	response = requests.get(url)
+	temp = (response.json()["main"]["temp"] - 273.15)* 1.8000 + 32.00
+	temp =  str(temp)
+	return render_template("temp.html", city=city, temp=temp)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- static/index.js
+++ static/index.js
@@ -1,3 +1,24 @@
 $(window).load(function () {
     $("#temp").fadeIn(3000);
 });
+
+$(document).ready(function () {
+	var ctx = document.getElementById("myChart").getContext("2d");
+
+	var data = {
+		labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+		datasets: [
+			{
+				label: "My First dataset",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: ['{{forcast_list[0]}}','{{forcast_list[1]}}','{{forcast_list[2]}}','{{forcast_list[3]}}',
+				'{{forcast_list[4]}}', '{{forcast_list[5]}}','{{forcast_list[6]}}',]
+			}]
+		};
+	var myLineChart = new Chart(ctx).Line(data);
+});
